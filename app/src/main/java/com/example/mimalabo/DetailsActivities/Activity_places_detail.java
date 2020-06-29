e863c771bd65a6040e6855e8970c17ddf8d05a0f
Francisco Mokuy <franciscodeasis50@gmail.com>
2020-06-29 03:51:37 -0500

    In this commit i added maps direction to the Activity_places_detail and also added an icon of direction to the toolbar as a a menu item.

--- app/src/main/java/com/example/mimalabo/DetailsActivities/Activity_places_detail.java
+++ app/src/main/java/com/example/mimalabo/DetailsActivities/Activity_places_detail.java
@@ -9,6 +9,8 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,7 +23,7 @@ import com.example.mimalabo.R;
 public class Activity_places_detail extends AppCompatActivity {
 
     public static final String EXTRA_HOTEL_ID1 = "placesId";
-    private static final String COORDINATES = "3.734893,8.7572579";
+    //private static final String COORDINATES = "3.734893,8.7572579";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -56,11 +58,32 @@ public class Activity_places_detail extends AppCompatActivity {
 
     }
 
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    //here we are making the item respond to clicks and if clicked we call the displayMap1() method.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.action_map)
+        {
+            displayMap(null);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     //this is the code to display the map
-    public void displayMap(View view) {
-        Uri uri = Uri.parse("geo:" + COORDINATES);
-        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-        startActivity(intent);
+    public void displayMap(View view)
+    {
+        for(int i=0;i<Places.places.length;i++)
+        {
+            Uri uri = Uri.parse("geo:" + Places.places[i]);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
 
     }
 }
