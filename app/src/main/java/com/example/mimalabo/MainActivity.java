d4911ce1ff3dd829b5c7756d8f4035ba1dfda517
Francisco Mokuy <franciscodeasis50@gmail.com>
2020-07-24 17:36:50 -0500

    We have added an animation to the MainActivity.java for the imageView.

--- app/src/main/java/com/example/mimalabo/MainActivity.java
+++ app/src/main/java/com/example/mimalabo/MainActivity.java
@@ -6,10 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private Button know;
+    private ImageView iconimage;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,10 +28,40 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        know = findViewById(R.id.know_malabo);
+        iconimage = findViewById(R.id.icon_image);
+        linearLayout = findViewById(R.id.linear_layout);
+
+        linearLayout.animate().alpha(0f).setDuration(100000);
+        TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        iconimage.setAnimation(animation);
+
 
     }
 
+    private class MyAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
 
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            iconimage.clearAnimation();
+            iconimage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
 
     public void KnowMalabo(View v)
     {
@@ -28,5 +69,6 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
 
     }
+
 
 }
